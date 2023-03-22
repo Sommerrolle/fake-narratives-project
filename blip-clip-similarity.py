009eef5086b385c7447e97887860a59c817045c8
Cuong Vo Ta <cuong.vota@mgm-tp.com>
2023-03-22 20:05:47 +0100

    Completing iterating-over-dataset function, adding ranking and filter function

--- blip-clip-similarity.py
+++ blip-clip-similarity.py
@@ -1,6 +1,7 @@
 from transformers import CLIPProcessor, CLIPModel
 import torch
 import json
+from operator import itemgetter
 
 # Using the clip-vit-base-patch32 model from openai
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
@@ -13,8 +14,8 @@ data_clip = json.load(f)
 f = open("blip.json")
 data_blip = json.load(f)
 
-def cos_sim_clip_blip(data_clip, data_blip):
 
+def cos_sim_clip_blip(data_clip, data_blip):
     image = data_clip['y']
     caption = data_blip['text']
 
@@ -22,52 +23,59 @@ def cos_sim_clip_blip(data_clip, data_blip):
     # add new dimension at position 0 to match shape of text tensor
     image_features = image_features.unsqueeze(0)
     image_features /= torch.linalg.norm(image_features, ord=2, dim=-1, keepdim=True)
-    #image_features /= image_features.norm(p=2, dim=-1, keepdim=True)
+    # image_features /= image_features.norm(p=2, dim=-1, keepdim=True)
 
     # blip caption
     text_input = processor(text=caption, return_tensors="pt")
     text_features = model.get_text_features(**text_input)
     text_features /= torch.linalg.norm(text_features, ord=2, dim=-1, keepdim=True)
-    #text_features /= text_features.norm(p=2, dim=-1, keepdim=True)
+    # text_features /= text_features.norm(p=2, dim=-1, keepdim=True)
 
     # compute cosine similarity
     # funktioniert aber kommen komische Ergenisse raus
     logit_scale = model.logit_scale.exp()
     similarity = torch.nn.functional.cosine_similarity(text_features, image_features) * logit_scale
 
-
-    return [{
+    return {
         "score": similarity.item(),
         "label": caption,
         "timestamp": data_clip['t']
-    }]
-
-    # sim = image_features.detach().numpy() @ text_features.detach().numpy().T
-    # return (sim * 100).softmax(dim=1)[0]
-
-    # geht auch mit der funktion
-    #similarity_score = torch.nn.functional.cosine_similarity(image_features, text_features)
-    #return similarity_score.item() * 100
+    }
 
 
 def iterate_over_data(data_clip, data_blip):
     results = []
     i = 0
-    for i in data_clip:
-        if data_blip[i]['begin'] <= data_clip[i]['t'] <= data_blip[i]['end']:
-            res = cos_sim_clip_blip(data_clip[i], data_blip[i])
-            results.append(res)
+    for index, blip_caption in enumerate(data_blip):
+        results_of_timeslot = []
+        # sometimes there is a time gap between the captions of blip
+        # therefore we need to step over the clip embedding
+        while data_clip[i]['t'] < blip_caption['begin']:
+            i += 1
+        while blip_caption['begin'] <= data_clip[i]['t'] <= blip_caption['end']:
+            if blip_caption['begin'] <= data_clip[i]['t'] <= blip_caption['end']:
+                res = cos_sim_clip_blip(data_clip[i], blip_caption)
+                results_of_timeslot.append(res)
+                i += 1
+        # just add the clib-emb and blip-caption combination with the best score
+        if len(results_of_timeslot) == 1:
+            results.append(results_of_timeslot[0])
+        elif len(results_of_timeslot) > 0:
+            res_sorted = rank_scores(results_of_timeslot)
+            results.append(res_sorted[0])
+    return results
+
+
+# Sorting results by score in descending order
+def rank_scores(results):
+    return sorted(results, key=itemgetter('score'), reverse=True)
 
 
 def main():
-    image_features = data_clip[0]
-    caption = data_blip[0]
-    result = cos_sim_clip_blip(image_features, caption)
-    print('result:')
-    print(result)
+    res = iterate_over_data(data_clip, data_blip)
+    res_ranked = rank_scores(res)
+    print(res_ranked)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
-
